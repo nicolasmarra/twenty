8e560864602c2d64299c0390a5835a90b715d7dc
nmarra <nicolas.marra@etu.unistra.fr>
2023-11-24 19:39:24 +0100

    enlever les lignes vides

--- src/twenty.cpp
+++ src/twenty.cpp
@@ -1,7 +1,7 @@
 #include <GL/glut.h>
-#include <iostream>
-#include <cstdlib>
 #include <cmath>
+#include <cstdlib>
+#include <iostream>
 
 #define numBlocksPerRow 7
 #define BLOCK_SIZE 0.18
@@ -10,9 +10,11 @@
 
 // On définit un espacement entre les blocs
 #define SPACING (1.6 / (numBlocksPerRow + 1))
-// On définit également un espacement sur la vertical et l'horizontal (à gauche et en bas)
+// On définit également un espacement sur la vertical et l'horizontal (à gauche
+// et en bas)
 
-#define BACKGROUND_LEFT (-0.8 + (1.6 - numBlocksPerRow * SPACING) / 2 + SPACING / 2)
+#define BACKGROUND_LEFT \
+    (-0.8 + (1.6 - numBlocksPerRow * SPACING) / 2 + SPACING / 2)
 #define BACKGROUND_BOTTOM (-0.8 + SPACING / 2)
 
 struct Block {
@@ -22,8 +24,8 @@ struct Block {
 };
 
 struct Color {
-	float r,g,b;
-	Color(float red, float green, float blue) : r(red), g(green), b(blue) {}
+    float r, g, b;
+    Color(float red, float green, float blue) : r(red), g(green), b(blue) {}
 };
 
 int nBlocks = N_BLOCK_INITIAL;
@@ -33,74 +35,102 @@ int score = 0, moves = 0;
 int elapsedTime = 0;
 bool stop_Time = false;
 
-Color getColor(int valeur)
-{
-	switch (valeur) {
-	        case 1:
-	            return Color(0.3, 0.8, 0.7); // bleu clair 1
-	        case 2:
-	            return Color(0.4, 0.3, 0.5); // violet foncé
-	        case 3:
-	            return Color(0.0, 0.0, 1.0); // bleu
-	        case 4:
-	            return Color(0.0, 0.5, 0.0); // vert
-	        case 5:
-	            return Color(1.0, 0.0, 0.0); // rouge
-	        case 6:
-	            return Color(0.0, 0.5, 0.5); // cyan
-	        case 7:
-	            return Color(0.5, 0.0, 0.5); // magenta
-	        case 8:
-	            return Color(0.5, 0.5, 0.0); // jaune
-	        case 9:
-	            return Color(0.5, 0.5, 0.5); // gris
-	        case 10:
-	            return Color(0.0, 1.0, 0.0); // vert vif
-	        case 11:
-	            return Color(1.0, 0.0, 1.0); // rose
-	        case 12:
-	            return Color(0.0, 1.0, 1.0); // turquoise
-	        case 13:
-	            return Color(1.0, 0.5, 0.0); // orange
-	        case 14:
-	            return Color(0.5, 1.0, 0.0); // vert clair
-	        case 15:
-	            return Color(0.0, 0.0, 0.5); // bleu foncé
-	        case 16:
-	            return Color(0.0, 0.5, 1.0); // bleu clair 2
-	        case 17:
-	            return Color(0.5, 0.0, 1.0); // violet
-	        case 18:
-	            return Color(1.0, 0.0, 0.5); // rouge clair
-	        case 19:
-	            return Color(0.5, 1.0, 0.5); // vert pâle
-	        case 20:
-	            return Color(0.5, 0.5, 1.0); // bleu pâle
-	        default:
-	            return Color(0.8, 0.0, 0.0); // rouge
-	    }
+Color getColor(int valeur) {
+    switch (valeur) {
+    case 1:
+        return Color(0.3, 0.8, 0.7); // bleu clair 1
+    case 2:
+        return Color(0.4, 0.3, 0.5); // violet foncé
+    case 3:
+        return Color(0.0, 0.0, 1.0); // bleu
+    case 4:
+        return Color(0.0, 0.5, 0.0); // vert
+    case 5:
+        return Color(1.0, 0.0, 0.0); // rouge
+    case 6:
+        return Color(0.0, 0.5, 0.5); // cyan
+    case 7:
+        return Color(0.5, 0.0, 0.5); // magenta
+    case 8:
+        return Color(0.5, 0.5, 0.0); // jaune
+    case 9:
+        return Color(0.5, 0.5, 0.5); // gris
+    case 10:
+        return Color(0.0, 1.0, 0.0); // vert vif
+    case 11:
+        return Color(1.0, 0.0, 1.0); // rose
+    case 12:
+        return Color(0.0, 1.0, 1.0); // turquoise
+    case 13:
+        return Color(1.0, 0.5, 0.0); // orange
+    case 14:
+        return Color(0.5, 1.0, 0.0); // vert clair
+    case 15:
+        return Color(0.0, 0.0, 0.5); // bleu foncé
+    case 16:
+        return Color(0.0, 0.5, 1.0); // bleu clair 2
+    case 17:
+        return Color(0.5, 0.0, 1.0); // violet
+    case 18:
+        return Color(1.0, 0.0, 0.5); // rouge clair
+    case 19:
+        return Color(0.5, 1.0, 0.5); // vert pâle
+    case 20:
+        return Color(0.5, 0.5, 1.0); // bleu pâle
+    default:
+        return Color(0.8, 0.0, 0.0); // rouge
+    }
 }
 
 void isVictory() {
-        glColor3f(1.0, 1.0, 1.0);
-        glRasterPos2f(-0.2, 0.0);
-        std::string victoryStr = "Vous avez gagné!";
-        for (char c : victoryStr) {
-            glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
-        }
+    glColor3f(1.0, 1.0, 1.0);
+    glRasterPos2f(-0.2, 0.0);
+    std::string victoryStr = "Vous avez gagné!";
+    for (char c : victoryStr) {
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
+    }
 
-        stop_Time = true;
+    stop_Time = true;
 
-        //On désactive la souris et le déplacement des blocs
-        glutMouseFunc(NULL);
-        glutMotionFunc(NULL);
+    // On désactive la souris et le déplacement des blocs
+    glutMouseFunc(NULL);
+    glutMotionFunc(NULL);
+}
+
+void new_score(int valeur) {
+    score = valeur > score ? valeur : score;
+    if (score == 20)
+        isVictory();
 }
 
 
-void new_score(int valeur)
-{
-		score = valeur > score ? valeur : score ;
-		if(score == 20) isVictory();
+void moveBlocks() {
+    for (int i = 0; i < nBlocks; i++) {
+        if (blocks[i].isVisible) {
+
+            for (int j = 0; j < nBlocks; j++) {
+
+            	Block tempBlock;
+            	tempBlock.x = blocks[i].x;
+            	tempBlock.y = blocks[i].y - SPACING;
+
+            	if (!blocks[j].isVisible && (blocks[j].x == tempBlock.x && blocks[j].y == tempBlock.y))
+            	{
+            		float temp_x = blocks[i].x;
+            		blocks[i].x = blocks[j].x;
+            		float temp_y = blocks[i].y;
+            		blocks[i].y = blocks[j].y;
+            		blocks[j].x = temp_x;
+            		blocks[j].y = temp_y;
+
+            		Block tempBlock1 = blocks[i];
+            		blocks[i] = blocks[j];
+            		blocks[j] = tempBlock1;
+            		break;
+                }
+            }
+    }
+    }
 }
 
 
@@ -113,63 +143,87 @@ void initBlocks() {
         int col = i % numBlocksPerRow; // On récupère la colonne actuelle
         blocks[i].x = BACKGROUND_LEFT + SPACING * col; // On répartit les blocs sur l'écran horizontalement
         blocks[i].y = BACKGROUND_BOTTOM + SPACING * row; // On répartit les blocs sur l'écran verticalement en bas
-        blocks[i].value = rand() % 5 + 1;				// Initialement les blocs ont de valeurs allant de 1 à 5
+        blocks[i].value = rand() % 5 + 1; // Initialement les blocs ont de valeurs allant de 1 à 5
         blocks[i].isVisible = true;
         new_score(blocks[i].value);
     }
 }
 
-void addBlocks() {
+int addBlocks() {
 
-    int row_nouveaux_blocs = 1;
-    int compteur_ligne = 0;
-    for (int i= N_BLOCK_INITIAL; i < nBlocks; i++)
-    {
-    	blocks[i].y = BACKGROUND_BOTTOM + SPACING * row_nouveaux_blocs; // On répartit les blocs sur l'écran verticalement en bas
-        compteur_ligne++;
-        if(compteur_ligne == numBlocksPerRow)
-        {
-        	compteur_ligne = 0;
-        	row_nouveaux_blocs++;
+    if (nBlocks < MAX_BLOCK) {
+        int row_nouveaux_blocs = 1;
+        int compteur_ligne = 0;
+        for (int i = N_BLOCK_INITIAL; i < nBlocks; i++) {
+
+            blocks[i].y =
+                BACKGROUND_BOTTOM +
+                SPACING * row_nouveaux_blocs; // On répartit les blocs sur
+                                              // l'écran verticalement en bas
+            compteur_ligne++;
+
+            if (compteur_ligne == numBlocksPerRow) {
+                compteur_ligne = 0;
+                row_nouveaux_blocs++;
+            }
+        }
+
+        for (int i = 0; i < N_BLOCK_INITIAL; ++i) {
+            // std::cout << "position en x: "<<blocks[i].x<<" position en y:
+            // "<<blocks[i].y<<std::endl;
+            int row = i / numBlocksPerRow;
+            row += row_nouveaux_blocs;
+            blocks[i].y = BACKGROUND_BOTTOM +
+                          SPACING * row; // On répartit les blocs sur l'écran
+                                         // verticalement en bas
+        }
+
+        for (int i = nBlocks; i < nBlocks + numBlocksPerRow; i++) {
+            int row = 0;                   // On récupère la ligne actuelle
+            int col = i % numBlocksPerRow; // On récupère la colonne actuelle
+            blocks[i].x =
+                BACKGROUND_LEFT +
+                SPACING *
+                    col; // On répartit les blocs sur l'écran horizontalement
+            blocks[i].y = BACKGROUND_BOTTOM +
+                          SPACING * row; // On répartit les blocs sur l'écran
+                                         // verticalement en bas
+            blocks[i].value =
+                rand() % 5 +
+                1; // Initialement les blocs ont de valeurs allant de 1 à 5
+            blocks[i].isVisible = true;
+            new_score(blocks[i].value);
+        }
+        nBlocks += numBlocksPerRow;
+    } else {
+        for (int i = 0; i < nBlocks; i++) {
+            if (blocks[i].y == 0.5 && blocks[i].isVisible)
+            {
+            	std::cout << "Défaite " << std::endl;
+            	return 0;
+            }
+
         }
     }
 
-    for (int i = 0; i < N_BLOCK_INITIAL; ++i) {
-        int row = i / numBlocksPerRow;
-        row += row_nouveaux_blocs;
-    	blocks[i].y = BACKGROUND_BOTTOM + SPACING * row; // On répartit les blocs sur l'écran verticalement en bas
-    }
-
-
-    for(int i =  nBlocks; i < nBlocks + numBlocksPerRow; i++)
-    {
-    	int row = 0; // On récupère la ligne actuelle
-    	int col = i % numBlocksPerRow; // On récupère la colonne actuelle
-    	blocks[i].x = BACKGROUND_LEFT + SPACING * col; // On répartit les blocs sur l'écran horizontalement
-    	blocks[i].y = BACKGROUND_BOTTOM + SPACING * row; // On répartit les blocs sur l'écran verticalement en bas
-    	blocks[i].value = rand() % 5 + 1;				// Initialement les blocs ont de valeurs allant de 1 à 5
-    	blocks[i].isVisible = true;
-    	new_score(blocks[i].value);
-
-    }
-    nBlocks += numBlocksPerRow;
+    moveBlocks();
+    return 1;
 }
-
-
 
 void mouse(int button, int state, int x, int y) {
     if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-        //On convertit les coordonnées de la souris en coordonnées OpenGL
+        // On convertit les coordonnées de la souris en coordonnées OpenGL
         float mouseX = (x / static_cast<float>(600)) * 2.0 - 1.0;
         float mouseY = 1.0 - (y / static_cast<float>(600)) * 2.0;
 
-        //On cherche le bloc le plus proche du clic
+        // On cherche le bloc le plus proche du clic
         int closestBlockIndex = -1;
         float closestDistance = std::numeric_limits<float>::max();
 
-        for (int i = 0; i < nBlocks ; ++i) {
+        for (int i = 0; i < nBlocks; ++i) {
             if (blocks[i].isVisible) {
-                float distance = sqrt(pow(mouseX - blocks[i].x, 2) + pow(mouseY - blocks[i].y, 2));
+                float distance = sqrt(pow(mouseX - blocks[i].x, 2) +
+                                      pow(mouseY - blocks[i].y, 2));
                 if (distance < closestDistance) {
                     closestBlockIndex = i;
                     closestDistance = distance;
@@ -178,59 +232,64 @@ void mouse(int button, int state, int x, int y) {
         }
 
         selectedBlockIndex = closestBlockIndex;
-
     }
 }
 
 void motion(int x, int y) {
     if (selectedBlockIndex != -1) {
-    	//std::cout << "Déplacement de la souris" << std::endl;
-    	//On Convertit les coordonnées de la souris en coordonnées OpenGL
+        // std::cout << "Déplacement de la souris" << std::endl;
+        // On Convertit les coordonnées de la souris en coordonnées OpenGL
 
-    	float mouseX = (x / static_cast<float>(600)) * 2.0 - 1.0;
-    	float mouseY = 1.0 - (y / static_cast<float>(600)) * 2.0;
+        float mouseX = (x / static_cast<float>(600)) * 2.0 - 1.0;
+        float mouseY = 1.0 - (y / static_cast<float>(600)) * 2.0;
 
+        for (int i = 0; i < nBlocks; ++i) {
+            if (i != selectedBlockIndex) {
+                float distance = sqrt(pow(blocks[i].x - mouseX, 2) +
+                                      pow(blocks[i].y - mouseY, 2));
+                if (distance < 0.18) {
+                    // On fusionne les blocs en ajoutant 1 à la valeur du bloc
+                    // déplacé
+                    if ((blocks[i].value == blocks[selectedBlockIndex].value) &&
+                        blocks[i].isVisible) {
+                        blocks[i].value += 1;
+                        blocks[selectedBlockIndex].isVisible = false;
+                        new_score(blocks[i].value);
+                        moves++;
+                    } else if (!blocks[i].isVisible) {
+                        // On déplace le bloc
+                        float temp_x = blocks[selectedBlockIndex].x;
+                        float temp_y = blocks[selectedBlockIndex].y;
+                        blocks[selectedBlockIndex].x = blocks[i].x;
+                        blocks[selectedBlockIndex].y = blocks[i].y;
+                        blocks[i].x = temp_x;
+                        blocks[i].y = temp_y;
+                        moves++;
 
-        for (int i = 0; i < nBlocks ; ++i) {
-    		if (i != selectedBlockIndex) {
-    			float distance = sqrt(pow(blocks[i].x - mouseX, 2) + pow(blocks[i].y - mouseY, 2));
-    			if (distance < 0.18) {
-    				//On fusionne les blocs en ajoutant 1 à la valeur du bloc déplacé
-    				if ((blocks[i].value == blocks[selectedBlockIndex].value) && blocks[i].isVisible) {
-    					blocks[i].value += 1;
-    					blocks[selectedBlockIndex].isVisible = false;
-    					new_score(blocks[i].value);
-    					moves++;
-    				}else if(!blocks[i].isVisible)
-    				{
-    					//On déplace le bloc
-    					float temp_x = blocks[selectedBlockIndex].x;
-    					float temp_y = blocks[selectedBlockIndex].y;
-    				    blocks[selectedBlockIndex].x = blocks[i].x;
-    				    blocks[selectedBlockIndex].y = blocks[i].y;
-    				    blocks[i].x = temp_x;
-    				    blocks[i].y = temp_y;
-    				    moves++;
-    				}
-    				break;
-    			}
-    		}
-    	}
+                        // Mise à jour des pointeurs
+                        Block tempBlock = blocks[selectedBlockIndex];
+                        blocks[selectedBlockIndex] = blocks[i];
+                        blocks[i] = tempBlock;
 
-      glutSwapBuffers();
-      glutPostRedisplay();
+                    }
+                    moveBlocks();
+                    break;
+                }
+            }
+        }
 
-      //On réinitialiser l'index du bloc sélectionné
-      selectedBlockIndex = -1;
+        glutSwapBuffers();
+        glutPostRedisplay();
 
+        // On réinitialiser l'index du bloc sélectionné
+        selectedBlockIndex = -1;
     }
 }
 
-
 void displayInfo() {
-	glColor3f(0.0, 0.0, 0.0);
+    glColor3f(0.0, 0.0, 0.0);
 
-	glRasterPos2f(-0.7, 0.82);
+    glRasterPos2f(-0.7, 0.82);
     std::string scoreStr = "Score: " + std::to_string(score);
     for (char c : scoreStr) {
         glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
@@ -244,11 +303,10 @@ void displayInfo() {
 
     glRasterPos2f(-0.7, 0.9);
     std::string tempsStr = "Temps: " + std::to_string(elapsedTime);
-        for (char c : tempsStr) {
-            glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
-        }
+    for (char c : tempsStr) {
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
+    }
 }
-
 
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
@@ -272,14 +330,14 @@ void display() {
     glEnd();
 
     for (int i = 0; i < nBlocks; ++i) {
-        const Block& block = blocks[i];
-        //On vérifie si le bloc est toujours visible
+        const Block &block = blocks[i];
+        // On vérifie si le bloc est toujours visible
         if (block.isVisible) {
             // On récupère la couleur associée à la valeur du bloc
             Color blockColor = getColor(block.value);
 
             glBegin(GL_QUADS);
-            glColor3f(blockColor.r, blockColor.g,blockColor.b);
+            glColor3f(blockColor.r, blockColor.g, blockColor.b);
 
             glVertex2f(block.x - BLOCK_SIZE / 2, block.y - BLOCK_SIZE / 2);
             glVertex2f(block.x + BLOCK_SIZE / 2, block.y - BLOCK_SIZE / 2);
@@ -287,7 +345,8 @@ void display() {
             glVertex2f(block.x - BLOCK_SIZE / 2, block.y + BLOCK_SIZE / 2);
             glEnd();
 
-            // On applique une petite bordure noire en bas du bloc pour le rendre plus beau
+            // On applique une petite bordure noire en bas du bloc pour le
+            // rendre plus beau
             glColor3f(0.0, 0.0, 0.0);
             glBegin(GL_LINES);
             glVertex2f(block.x - BLOCK_SIZE / 2, block.y - BLOCK_SIZE / 2);
@@ -296,7 +355,9 @@ void display() {
 
             // Pour finir, on affiche la valeur associée au bloc
             glColor3f(1.0, 1.0, 1.0);
-            glRasterPos2f(block.x - 0.02, block.y - 0.02); // La position ou* on affichera la valeur
+            glRasterPos2f(block.x - 0.02,
+                          block.y -
+                              0.02); // La position ou* on affichera la valeur
             std::string valueStr = std::to_string(block.value);
             for (char c : valueStr) {
                 glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
@@ -311,51 +372,48 @@ void display() {
 
 void timer(int value) {
 
-	if(!stop_Time)
-	{
-	elapsedTime++;
+    if (!stop_Time) {
+        elapsedTime++;
 
-
-    if (elapsedTime == 15) {
-        //addBlocks(nouvelle_ligne);
-    	addBlocks();
-        elapsedTime = 0;
+        if (elapsedTime == 15) {
+            // addBlocks(nouvelle_ligne);
+            addBlocks();
+            elapsedTime = 0;
+        }
     }
-	}
-    //On configure le prochain appel à la fonction timer
+    // On configure le prochain appel à la fonction timer
     glutPostRedisplay();
     glutTimerFunc(1000, timer, 0);
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
 
-	//on initialise FreeGLUT
-	glutInit(&argc, argv);
+    // on initialise FreeGLUT
+    glutInit(&argc, argv);
 
-	//on configure le display mode
-	glutInitDisplayMode ( GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+    // on configure le display mode
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
 
-	//on configure la position de la fenêtre
-	glutInitWindowPosition(200,50);
+    // on configure la position de la fenêtre
+    glutInitWindowPosition(200, 50);
 
-	//on configure configure la taille de la fenêtre
-	glutInitWindowSize(600,600);
+    // on configure configure la taille de la fenêtre
+    glutInitWindowSize(600, 600);
 
-	// et on crée la fenêtre
-	glutCreateWindow ("TWENTY");
+    // et on crée la fenêtre
+    glutCreateWindow("TWENTY");
 
-	//on définit la fonction display
-	glutDisplayFunc(display);
-	glutMouseFunc(mouse);
-	glutMotionFunc(motion);
+    // on définit la fonction display
+    glutDisplayFunc(display);
+    glutMouseFunc(mouse);
+    glutMotionFunc(motion);
 
+    glutTimerFunc(1000, timer, 0);
 
-	glutTimerFunc(1000, timer, 0);
+    // on initialise le générateur de la valeur aléatoire avec l'heure courante
+    srand(time(nullptr));
 
-	//on initialise le générateur de la valeur aléatoire avec l'heure courante
-	srand(time(nullptr));
-
-	initBlocks(); // on initialise les blocs
+    initBlocks(); // on initialise les blocs
 
     glutMainLoop();
     return 0;
